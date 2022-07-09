a691946da34b3ea3f7024fee04a3b0f021d917a0
yasarertekin <yasarertekin@outlook.com>
2022-07-09 12:33:42 +0200

    main - Adjust LoadDatabase with service implementation

--- api/src/main/java/nl/rabo/banking/configuration/LoadDatabase.java
+++ api/src/main/java/nl/rabo/banking/configuration/LoadDatabase.java
@@ -2,7 +2,7 @@ package nl.rabo.banking.configuration;
 
 import nl.rabo.banking.entity.Account;
 import nl.rabo.banking.model.Card;
-import nl.rabo.banking.service.AccountService;
+import nl.rabo.banking.service.impl.AccountServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -15,11 +15,11 @@ import java.math.BigDecimal;
 public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
     @Bean
-    CommandLineRunner initDatabase(AccountService accountService) {
+    CommandLineRunner initDatabase(AccountServiceImpl accountServiceImpl) {
 
         return args -> {
-            log.info("Preloading " + accountService.save(new Account(0l,"1", Card.DEBIT.toString(), BigDecimal.TEN)));
-            log.info("Preloading " + accountService.save(new Account(0l,"2", Card.CREDIT.toString(), BigDecimal.TEN)));
+            log.info("Preloading {}", accountServiceImpl.save(new Account(0L,"1", Card.DEBIT.name(), BigDecimal.TEN)));
+            log.info("Preloading {}", accountServiceImpl.save(new Account(0L,"2", Card.CREDIT.name(), BigDecimal.TEN)));
         };
     }
 }
